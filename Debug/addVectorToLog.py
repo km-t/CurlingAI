e81c2256c261f2712135e3a4fd6ec9e0cc4bbce9
km-t <km-t-1014-fms@docomo.ne.jp>
2019-05-06 11:22:59 +0900

    Revert "Revert "Update addVectorToLog.py""
    This reverts commit 0283a663a55a580a6e9bc6bf5f5913d63fd219eb.

--- Debug/addVectorToLog.py
+++ Debug/addVectorToLog.py
@@ -13,6 +13,7 @@ for filename in file_list:
     
     with open(filename, "w") as a:
         a.write("")
+    with open(filename, "a")as ff:
         for line in lines:
             line = file + line
             ff.write(line)
